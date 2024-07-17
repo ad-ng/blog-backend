4a6ac89d9063e7a255fec3ca5cbfebab64609693
ad-ng <you@example.com>
2024-07-17 12:02:14 +0200

    defining users model

---
+++ server/model/user_model.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const userschema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports = mongoose.model('users',userschema)
